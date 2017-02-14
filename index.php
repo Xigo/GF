e72faf79d75612d460425fab5da8d815c92d8b77
Xigo <goxyzvrk011@gmail.com>
2017-02-14 19:40:26 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+class User{
+
+
+public $a;
+public $b;
+
+
+          function __construct($a,$b){
+
+          $this->a = $a;
+          $this->b = $b;
+
+          }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
